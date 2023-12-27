a4fdc49fba4902fa1506299e0d70b8439761cce9
chocheolhee <whcjfgml92@naver.com>
2023-12-27 21:08:25 +0900

    refactor: 배포 전 프로젝트 정리

--- src/main/java/com/toy/knit/config/WebConfig.java
+++ src/main/java/com/toy/knit/config/WebConfig.java
@@ -3,10 +3,11 @@ package com.toy.knit.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
